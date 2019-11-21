df489c17b886a08e9702593f562ecfbef0984efd
Camille Brondel <camille.brondel@utbm.fr>
2019-11-21 20:48:30 +0100

    last save

---
+++ LeJeu/Source/LeJeu/Public/Ble.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/WorldSettings.h"
+#include "Ble.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LEJEU_API ABle : public AWorldSettings
+{
+	GENERATED_BODY()
+	
+};
